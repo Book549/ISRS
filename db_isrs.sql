baa4473b2288a96addaeb33dd0c8aa7cfa878d10
Wattanachai Maijampa <wmaijampa549@gmail.com>
2024-09-04 16:21:30 +0700

    update database
    add more columm

--- db_isrs.sql
+++ db_isrs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2024 at 10:52 AM
+-- Generation Time: Sep 04, 2024 at 11:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -31,49 +31,9 @@ CREATE TABLE `colors` (
   `id_color` int(2) NOT NULL,
   `color_name` varchar(128) NOT NULL,
   `color_color` varchar(32) NOT NULL,
+  `color_ president` varchar(32) NOT NULL,
+  `color_vice-president` varchar(32) NOT NULL,
   `color_id_user` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `players`
---
-
-CREATE TABLE `players` (
-  `id_player` int(6) NOT NULL,
-  `player_name` varchar(128) NOT NULL,
-  `player_note` varchar(128) NOT NULL,
-  `player_id_sport` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sports`
---
-
-CREATE TABLE `sports` (
-  `id_sport` int(6) NOT NULL,
-  `sport_type` varchar(32) NOT NULL,
-  `sport_degree` varchar(32) NOT NULL,
-  `sport_gender` varchar(32) NOT NULL,
-  `sport_amount` int(3) NOT NULL,
-  `sport_id_color` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id_user` int(6) NOT NULL,
-  `user_username` varchar(32) NOT NULL,
-  `user_password` varchar(32) NOT NULL,
-  `user_role` varchar(32) NOT NULL,
-  `user_name` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -87,24 +47,6 @@ ALTER TABLE `colors`
   ADD PRIMARY KEY (`id_color`);
 
 --
--- Indexes for table `players`
---
-ALTER TABLE `players`
-  ADD PRIMARY KEY (`id_player`);
-
---
--- Indexes for table `sports`
---
-ALTER TABLE `sports`
-  ADD PRIMARY KEY (`id_sport`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -112,25 +54,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `id_color` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `players`
---
-ALTER TABLE `players`
-  MODIFY `id_player` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sports`
---
-ALTER TABLE `sports`
-  MODIFY `id_sport` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_color` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
