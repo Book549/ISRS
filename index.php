50eff0360c618c90c0e8ddc06fd98ee95a859545
Wattanachai Maijampa <wmaijampa549@gmail.com>
2024-10-11 12:04:13 +0700

    base front-end
    add basic stuff

--- index.php
+++ index.php
@@ -1,25 +1,14 @@
 <?php include 'conn.php'; ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-<?php echo "This is a test <br>"; 
-	$sql = "SELECT * FROM `test`";
-	$resuit = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($resuit) > 0 ) {
-		while ($row = mysqli_fetch_assoc($resuit)) {
-			foreach ($row as $key => $value) {
-				echo "$key -- $value <br>";
-				echo "--------------------<br>";
-			}
-		}
-	}else{
-		echo "look like it empty";
-	}
-?>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>admin dashboard</title>
+	</head>
+	<body>
+	<?php include 'side_bar.php' ?>
+
+
+	</body>
 </html>
