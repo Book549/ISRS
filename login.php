09c0ea28e0468ba408e82a374555bc9336d35018
Book549 <wmaijampa549@gmail.com>
2024-10-31 01:36:46 +0700

    fix bug
    fix bug in login page

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet"  href="element/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<title>Log in</title>
 </head>
@@ -15,23 +15,25 @@
 	<div class="login-all">
 	<a href="index.php"><img src="http://samakkhi.ac.th/wp-content/uploads/2022/07/swk-150x150.png" width="100px" height="100px"></a>
 		<div class="login-group">
-
+			
+		
+		
 			<h5>ลงทะเบียนกีฬาสี</h5>
+           
 
-			<form method="post" class="login-form" action="">
+			<form method="post" class="login-form">
 				<div>
-					<label for="login-user">Username :</label><br>
-					<input type="text" name="username"  id="login-user" class="box" placeholder="Enter your username"><br>
+				<label for="login-user">Username </label><br>
+				<input type="text" name="username"  id="login-user" class="box" placeholder=" Enter your username" required><br>
 				</div>
 				<div>
-					<label for="login-pass">Password :</label><br>
-					<input type="password" name="password"  id="login-pass" class="box" placeholder="Enter your password"  required><br>
-				</div>
-				<div >
-					<i class="fa-solid fa-eye-slash" id="showpassword"></i>
+				<label for="login-pass">Password </label><br>
+				<div class="password">
+				<input type="password" name="password"  id="login-pass" class="box" placeholder=" Enter your password"  required ><br> 
+				<i class="fa-solid fa-eye-slash" id="showpassword"></i>
 				</div>
 				<div style="display: flex; justify-content: center;">
-					<input type="submit" name="login"  class="btn" value="Log in">
+				<input type="submit" name="login"  class="btn" value="Log in">
 				</div>
 				
 			</form>
@@ -62,7 +64,8 @@ if ($_POST) {
 		echo "err";
 	}
 }
-switch ($_SESSION['user_role']) {
+if (isset($_SESSION['user_role'])) {
+	switch ($_SESSION['user_role']) {
 	case 'admin_system':
 		header("Location: admin_system.php");
 		break;
@@ -78,7 +81,9 @@ switch ($_SESSION['user_role']) {
 	default:
 		// code...
 		break;
+	}
 }
+
 ?>
 </body>
 </html>
