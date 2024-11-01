fbad5119c2013b806c2cf0aeae3d4552e097f2b2
bluemzzn <synthiawithsyren@gmail.com>
2024-11-01 22:17:02 +0700

    Update admin_system.php

--- admin_system.php
+++ admin_system.php
@@ -1,3 +1,4 @@
+
 <?php 
   include 'conn.php'; 
   if ($_SESSION['user_role'] !== 'admin_system') {
@@ -5,6 +6,7 @@
       exit();
   }
 ?>
+  
 <!DOCTYPE html>
 <html lang="th">
 <head>
@@ -30,7 +32,7 @@
       </li>
       <li class="active">
         <a href="?page=main_page">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-200h120v-200q0-17 11.5-28.5T400-440h160q17 0 28.5 11.5T600-400v200h120v-360L480-740 240-560v360Zm-80 0v-360q0-19 8.5-36t23.5-28l240-180q21-16 48-16t48 16l240 180q15 11 23.5 28t8.5 36v360q0 33-23.5 56.5T720-120H560q-17 0-28.5-11.5T520-160v-200h-80v200q0 17-11.5 28.5T400-120H240q-33 0-56.5-23.5T160-200Zm320-270Z"/></svg>
+        <i class="fa-solid fa-house" style="padding-right:10px;"></i>
           <span>หน้าหลัก</span>
         </a>
       </li>
@@ -69,20 +71,20 @@
       </li>
       <li>
         <a href="?page=results">
-		<i class="fa-solid fa-trophy" style="padding-right: 5px;"></i>
-          <span>ผลการแข่งขัน</span>
+		<i class="fa-solid fa-trophy" ></i>
+          <span style="padding-left: 7px;">ผลการแข่งขัน</span>
         </a>
       </li>
       <li>
         <a href="?page=certificate">
-		<i class="fa-solid fa-award" style="padding-right: 9px;"></i>
-          <span>เกียรติบัตร</span>
+		<i class="fa-solid fa-award" ></i>
+          <span style="padding-left: 12px;">เกียรติบัตร</span>
         </a>
       </li>
 	  <li>
         <a href="?page=log_out">
-		<i class="fas fa-sign-out-alt" style="padding-right: 5px;"></i>
-          <span>ออกจากระบบ</span>
+		    <i class="fas fa-sign-out-alt" ></i>
+          <span style="padding-left: 10px;">ออกจากระบบ</span>
         </a>
       </li>
     </ul>
