b4841cbc0090b06edf028c43b580f7128ab78bb0
bluemzzn <synthiawithsyren@gmail.com>
2024-11-05 14:38:25 +0700

    Update view_add_sport.php

--- admin_sport/view/view_add_sport.php
+++ admin_sport/view/view_add_sport.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>เพิ่มจำนวนนักกีฬา</title>
+	<link rel="stylesheet"  href="element/styles_admin_sport.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 <body>
-	<table>
+	<center>
+	<table class="table_all">
 		<tr>
 			<th>sport_id</th>
 			<th>sport_name</th>
@@ -38,4 +52,6 @@
 		 ?>
 
 	</table>
+	</center>
 </body>
+</html>
