e696a2e77cfb9b0bd624fea8742f673abea9da6c
bluemzzn <synthiawithsyren@gmail.com>
2024-11-10 22:25:12 +0700

    Add files via upload

---
+++ admin_report/add/add_report_all.php
@@ -0,0 +1,13 @@
+<body>
+<form method="post" class="add_sport_all">
+        <label for="report_rank">อันดับ:</label> <input type="text" class="box_sport" name="report_rank" id="report_rank"><br>
+		<label for="report_color">คณะสี:</label> <input type="text" class="box_sport" name="report_color" id="report_color"><br>
+		<label for="report_gold">🥇Gold:</label> <input type="text" class="box_sport" name="report_gold" id="report_gold"><br>
+		
+		<label for="report_silver">🥈Silver:</label> <input type="text" class="box_sport" name="report_silver" id="report_silver"><br>
+		<label for="report_bronze">🥉Bronze:</label> <input type="text" class="box_sport" name="report_bronze" id="report_bronze"><br>	
+		<label for="report_total">รวม:</label> <input type="text" class="box_sport" name="report_total" id="report_total"><br>
+		
+		<input type="submit" name="add_player" class="btn">
+</form>
+<
