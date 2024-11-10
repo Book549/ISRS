60564d9433aa7e389726727b6473fca0f982e559
bluemzzn <synthiawithsyren@gmail.com>
2024-11-10 22:24:35 +0700

    Create add_report_sport.php

---
+++ admin_report/add/add_report_sport.php
@@ -0,0 +1,75 @@
+<body>
+	<center>
+	<div class="add_sport_all">
+	<form method="post" >
+    <table>
+  			<label>รายการกีฬา:</label>
+  			<select>
+  				<option value="ฟุตบอล">ฟุตบอล</option>
+  				<option value="ฟุตซอล"> ฟุตซอล</option>
+  				<option value="แฮนด์บอล">แฮนด์บอล</option>
+  				<option value="วอลเลย์บอล">วอลเลย์บอล</option>
+				<option value="เปตอง"> เปตอง</option>
+  				<option value="บาสเก็ตบอล">บาสเก็ตบอล</option>
+  				<option value="เทเบิลเทนนิส">เทเบิลเทนนิส</option>
+				<option value="กีฬาพื้นบ้าน"> กีฬาพื้นบ้าน</option>
+  				<option value="กรีฑา">กรีฑา</option>
+  				<option value="ม.1 บอลมหาชัย">ม.1 บอลมหาชัย</option>
+                <option value="ม.2 แอโรบิก">ม.2 แอโรบิก</option>
+  				<option value="ม.3 Boxing Kids">ม.3 Boxing Kids</option>
+                <option value="ม.4 จักรยานคนจน">ม.4 จักรยานคนจน</option>
+  			</select>
+  		</table>
+
+        <h3>ผลการแข่งขัน:</h3>
+          <table>
+  			<label>🥇1</label>
+  			<select>
+  				<option value="ชมพู">ชมพู</option>
+  				<option value="ฟ้า"> ฟ้า</option>
+  				<option value="เหลือง">เหลือง</option>
+  				<option value="เขียว">เขียว</option>
+				<option value="แดง"> แดง</option>
+  			</select>
+
+              <label>🥈2</label>
+  			<select>
+  				<option value="ชมพู">ชมพู</option>
+  				<option value="ฟ้า"> ฟ้า</option>
+  				<option value="เหลือง">เหลือง</option>
+  				<option value="เขียว">เขียว</option>
+				<option value="แดง"> แดง</option>
+  			</select>
+
+              <label>🥉3</label>
+  			<select>
+  				<option value="ชมพู">ชมพู</option>
+  				<option value="ฟ้า"> ฟ้า</option>
+  				<option value="เหลือง">เหลือง</option>
+  				<option value="เขียว">เขียว</option>
+				<option value="แดง"> แดง</option>
+  			</select>
+
+              <label>4</label>
+  			<select>
+  				<option value="ชมพู">ชมพู</option>
+  				<option value="ฟ้า"> ฟ้า</option>
+  				<option value="เหลือง">เหลือง</option>
+  				<option value="เขียว">เขียว</option>
+				<option value="แดง"> แดง</option>
+  			</select>
+            
+              <label>5</label>
+  			<select>
+  				<option value="ชมพู">ชมพู</option>
+  				<option value="ฟ้า"> ฟ้า</option>
+  				<option value="เหลือง">เหลือง</option>
+  				<option value="เขียว">เขียว</option>
+				<option value="แดง"> แดง</option>
+  			</select>
+  		</table>
+		<input type="submit" name="add_player" class="btn">
+	</form>
+	</div>
+	</center>
+</body>
