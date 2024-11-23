99518508a24d520add088ad1713cc6ca262ec8b4
Book549 <wmaijampa549@gmail.com>
2024-11-23 20:21:50 +0700

    small update

--- form.php
+++ form.php
@@ -20,7 +20,7 @@ if (isset($_GET['sport']) && $_GET['sport'] != "main") {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title></title>
+    <title>ฟอร์มลงทะเบียนนักกีฬา</title>
 </head>
 	<center>
 <body><!-- จำเป็นต้องตอบคำถามนี้ -->
@@ -132,6 +132,8 @@ if (isset($_GET['sport']) && $_GET['sport'] != "main") {
 		<option value="Y.8">Y.8</option>
 		<option value="Y.9">Y.9</option>
 		<option value="Y.10">Y.10</option>
+		<option value="Y.11">Y.11</option>
+		<option value="Y.12">Y.12</option>
 	</select>
 	<br>
 <!--		<label for="player_gender">เพศ :</label>
